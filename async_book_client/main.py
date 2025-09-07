e3a983acb47b3f7dfdbcf7751b03482bc1a4a090
AmirMacsen <3254168608@qq.com>
2025-09-07 12:08:38 +0800

    feature: grpc async interceptor

--- async_book_client/main.py
+++ async_book_client/main.py
@@ -1,10 +1,19 @@
 import grpc
+from grpc import intercept_channel
 
 from async_book_client import book_pb2_grpc, book_pb2
 
+## 客户端拦截器 异步版本
+class MyInterceptor(grpc.aio.UnaryUnaryClientInterceptor):
+    # 实现这个方法
+    async def intercept_unary_unary(self, continuation, client_call_details, request):
+        print("interceptor")
+        return await continuation(client_call_details, request)
+
 
 async def main():
-    async with grpc.aio.insecure_channel('localhost:50051') as channel:
+    # 客户端拦截器
+    async with grpc.aio.insecure_channel('localhost:50051', interceptors=(MyInterceptor(),)) as channel:
         stub = book_pb2_grpc.BookServiceStub(channel)
         # 异步调用不是with_call
         call = stub.List(
--- async_book_server/main.py
+++ async_book_server/main.py
@@ -7,7 +7,24 @@ from async_book_server import book_pb2, book_pb2_grpc
 
 """
 元数据： 类似http请求中的header，可以上下文中获取或者添加元数据，实现权限校验或者客户端服务端一些特殊数据的传递
+
+拦截器：通过继承grpc  实现对请求的拦截
 """
+
+class BookServiceInterceptor(grpc.aio.ServerInterceptor):
+    async def intercept_service(self, continuation, handler_call_details):
+        print("进入第一个拦截器...")
+        next_handler = await continuation(handler_call_details)
+        print("离开第一个拦截器...")
+        return next_handler
+
+class BookServiceInterceptor2(grpc.aio.ServerInterceptor):
+    async def intercept_service(self, continuation, handler_call_details):
+        print("进入第二个拦截器...")
+        next_handler = await continuation(handler_call_details)
+        print("离开第二个拦截器...")
+        return next_handler
+
 
 class BookService(book_pb2_grpc.BookServiceServicer):
     def List(self, request, context):
@@ -43,7 +60,7 @@ class BookService(book_pb2_grpc.BookServiceServicer):
         return response
 
 async def main():
-    server = grpc.aio.server()
+    server = grpc.aio.server(interceptors=[BookServiceInterceptor(), BookServiceInterceptor2()])
     book_pb2_grpc.add_BookServiceServicer_to_server(BookService(), server)
     server.add_insecure_port('localhost:50051')
     await server.start()
